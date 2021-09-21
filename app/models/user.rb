ffbc215c71250d028e28ff57cbf007e0ea2774fc
RayO <ray.oma360@gmail.com>
2021-09-20 21:47:00 -0700

    Followers table with references

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	has_many :users, class_name: 'Follower', foreign_key: 'user_id'
+	has_many :followed_user, class_name: 'Follower', foreign_key: 'followed_user_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_045252) do
+ActiveRecord::Schema.define(version: 2021_09_21_030722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "followed_user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_user_id"], name: "index_followers_on_followed_user_id"
+    t.index ["user_id"], name: "index_followers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -26,4 +35,6 @@ ActiveRecord::Schema.define(version: 2021_09_16_045252) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "followers", "users"
+  add_foreign_key "followers", "users", column: "followed_user_id"
 end
