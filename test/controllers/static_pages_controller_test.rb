91d5384eb69fcec2a993258b6ed6df633598394d
RayO <ray.oma360@gmail.com>
2021-10-04 21:40:26 -0700

    Troubleshooting testing errors

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Bravo"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Bravo"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Bravo"
   end
 end
