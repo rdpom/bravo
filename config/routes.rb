ba6e371cd9cad98358216faa1dcfc3a86f02ce3e
RayO <ray.oma360@gmail.com>
2021-10-26 14:26:30 -0700

    middle of CH. 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  # get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  root                'static_pages#home'
+  get 'help'      =>  'static_pages#help'
+  get 'about'     =>  'static_pages#about'
+  get 'contact'   =>  'static_pages#contact'
+
+  get 'static_pages/home'
   get 'user/display'
   get 'user/add'
-  # get 'hello_world/index'
+  get 'hello_world/index'
   # root 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,22 +3,28 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     # get static_pages_home_url
-    get '/static_pages/home'
+    get '/'
     assert_response :success
-    assert_select "title", "Home | Bravo App"
+    assert_select "title", "Bravo"
   end
 
   test "should get help" do
     # get static_pages_help_url
     get '/static_pages/help'
     assert_response :success
-    assert_select "title", "Help | Bravo App"
+    assert_select "title", "Help | Bravo"
   end
 
   test "should get about" do
 #     get static_pages_about_url
     get '/static_pages/about'
     assert_response :success
-    assert_select "title", "About | Bravo App"
+    assert_select "title", "About | Bravo"
+  end
+
+    test "should get contact" do
+    get '/static_pages/contact'
+    assert_response :success
+    assert_select "title", "Contact | Bravo"
   end
 end
