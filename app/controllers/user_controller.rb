c3430e2a41ba5eab5c388c93a5b393b9f424feef
RayO <55900432+rdpom@users.noreply.github.com>
2021-10-26 16:03:27 -0700

    Static pages (#6)
    * add a static pages controller
    
    * Finished adding static pages
    
    * Simple css (#5)
    
    * Troubleshooting testing errors
    
    * Cleaning up tests and removing sample app references
    
    * middle of CH. 5
    
    * Finish layout and routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,10 @@ class UserController < ApplicationController
   def display
     @user = User.first 
   end
+
+  def add
+  end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  root                'static_pages#home'
+  get 'help'      =>  'static_pages#help'
+  get 'about'     =>  'static_pages#about'
+  get 'contact'   =>  'static_pages#contact'
+
+  get 'static_pages/home'
   get 'user/display'
-  root 'hello_world#index'
+  get 'user/add'
+  get 'signup'  => 'user#new'
+
+  get 'hello_world/index'
+  # root 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
