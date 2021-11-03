d2ca062c2cc0ede50d49bc8706dc2d38cb027c79
RayO <ray.oma360@gmail.com>
2021-11-02 22:00:14 -0700

    Finished user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def display
+    @user = User.first 
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def add
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Bravo!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+end
