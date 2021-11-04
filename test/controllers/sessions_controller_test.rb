e09041f876cd90f7d9ff782c0035d024395a355e
RayO <ray.oma360@gmail.com>
2021-11-03 18:28:48 -0700

    Log-in-log-out

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get New" do
+    get sessions_New_url
+    assert_response :success
+  end
+end
