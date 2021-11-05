e50c349d78814590c005042c06adb5cf02cebebb
RayO <55900432+rdpom@users.noreply.github.com>
2021-11-04 17:31:49 -0700

    Finish user edit, update, index, and destroy actions (#9)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create(name: 'Luke', birthday: Time.current, email: 'fake@fake.com', password: 'test')
-user2 = User.create(name: 'John', birthday: Time.current, email: 'real@real.com', password: 'test2')
-Follower.create(followee_id: user1.id, follower_id: user2.id)
+
+# my examples
+# user1 = User.create(name: 'Luke', birthday: Time.current, email: 'fake@fake.com', password: 'test')
+# user2 = User.create(name: 'John', birthday: Time.current, email: 'real@real.com', password: 'test2')
+# Follower.create(followee_id: user1.id, follower_id: user2.id)
+
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
