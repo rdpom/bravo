654cd54d98a94c4deb49196678c629418a976b85
RayO <ray.oma360@gmail.com>
2021-11-04 22:13:56 -0700

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 
   get 'hello_world/index'
   # root 'hello_world#index'
