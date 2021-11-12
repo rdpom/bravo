34a1a728c13579afc474705b42b58c55a1dd8a3e
RayO <ray.oma360@gmail.com>
2021-11-11 20:10:24 -0800

    Fixing branch mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/New'
   root                'static_pages#home'
-  get 'help'      =>  'static_pages#help'
-  get 'about'     =>  'static_pages#about'
-  get 'contact'   =>  'static_pages#contact'
-
-  get 'static_pages/home'
-  get 'users/display'
-  get 'users/add'
-  get 'signup'  => 'users#new'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
-
-  get 'hello_world/index'
-  # root 'hello_world#index'
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_001307) do
+ActiveRecord::Schema.define(version: 2021_11_12_025558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_11_05_001307) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followee_id"], name: "index_followers_on_followee_id"
     t.index ["follower_id"], name: "index_followers_on_follower_id"
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_11_05_001307) do
 
   add_foreign_key "followers", "users", column: "followee_id"
   add_foreign_key "followers", "users", column: "follower_id"
+  add_foreign_key "microposts", "users"
 end
