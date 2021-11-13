3699f210c29d0099b4d3a7e74e5eb9e212bc8363
RayO <ray.oma360@gmail.com>
2021-11-13 12:57:03 -0800

    fixed account verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,20 @@
 class User < ApplicationRecord
 	has_many :microposts, dependent: :destroy
-	attr_accessor :remember_token
-	before_save { self.email = email.downcase }
+	has_secure_password
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	
+	attr_accessor :remember_token, :activation_token
 	validates :name, presence: true
 	validates :email, presence: true
 	validates :name,  presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
  	validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+	before_create :create_activation_digest
+	before_save :downcase_email
 
 	# Returns the hash digest of the given string.
   	def User.digest(string)
@@ -93,6 +97,7 @@ class User < ApplicationRecord
 
 	    # Creates and assigns the activation token and digest.
 	    def create_activation_digest
+	    	# binding.pry
 	      self.activation_token  = User.new_token
 	      self.activation_digest = User.digest(activation_token)
 	    end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_045524) do
+ActiveRecord::Schema.define(version: 2021_11_12_025558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2021_11_12_045524) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
