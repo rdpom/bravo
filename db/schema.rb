ac67bcae854996df5fd7457bdd5dea7827ffb7bb
RayO <55900432+rdpom@users.noreply.github.com>
2021-11-13 23:07:08 -0800

    Password reset fix (#14)
    * fixed account verification
    
    * seed fix
    
    * email fix test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_12_045524) do
+ActiveRecord::Schema.define(version: 2021_11_14_064335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
